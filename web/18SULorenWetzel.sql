4408f39afe4bc241c1f23d7fc36d9387404e0d82
lw244130 <lw244130@LR00912.southeast.edu>
2018-09-04 02:55:03 -0500

    new database

--- web/18SULorenWetzel.sql
+++ web/18SULorenWetzel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2018 at 06:01 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: Sep 04, 2018 at 09:46 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,26 +36,30 @@ CREATE TABLE `persons` (
   `MiddleName` varchar(20) DEFAULT NULL,
   `LastName` varchar(50) NOT NULL,
   `BirthDate` date NOT NULL,
-  `HireDate` date NOT NULL
+  `HireDate` date NOT NULL,
+  `Salary` decimal(10,2) DEFAULT NULL,
+  `Rate` decimal(10,2) DEFAULT NULL,
+  `AvgWeeklyHours` decimal(10,2) DEFAULT NULL,
+  `EmployeeType` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `persons`
 --
 
-INSERT INTO `persons` (`EmployeeID`, `FirstName`, `MiddleName`, `LastName`, `BirthDate`, `HireDate`) VALUES
-(1, 'Loren', 'R', 'Wetzel', '1981-08-09', '2018-08-07'),
-(3, 'Ashton', 'M', 'Wetzel', '2015-02-14', '2018-02-26'),
-(16, 'Romeo', 'R', 'Wetzel', '2002-04-25', '2010-04-30'),
-(37, 'Loren', 'R', 'Wetzel', '1981-08-09', '1999-08-01'),
-(65, 'Aaron', 'A', 'Aaronson', '1980-01-01', '2013-01-02'),
-(66, 'Erin', 'E', 'Erinson', '1980-01-01', '2012-01-01'),
-(734, 'Roy', '', 'Batty', '2016-01-08', '2016-01-09'),
-(1313, 'Beatrix', '', 'Kiddo', '1976-02-28', '2003-10-10'),
-(1337, 'Molly', '', 'Millions', '1984-07-01', '2000-04-18'),
-(1985, 'Marty', '', 'McFly', '1968-06-12', '1885-01-01'),
-(2000, 'Paul', 'Muad\'Dib', 'Atreides', '1965-04-04', '1984-05-05'),
-(2001, 'Some', 'One', 'Else', '1999-07-07', '2018-08-27');
+INSERT INTO `persons` (`EmployeeID`, `FirstName`, `MiddleName`, `LastName`, `BirthDate`, `HireDate`, `Salary`, `Rate`, `AvgWeeklyHours`, `EmployeeType`) VALUES
+(1, 'Loren', 'R', 'Wetzel', '1981-08-09', '2018-08-07', NULL, NULL, NULL, 'None'),
+(3, 'Ashton', 'M', 'Wetzel', '2015-02-14', '2018-02-26', '150000.00', NULL, NULL, 'Salary'),
+(16, 'Romeo', 'R', 'Wetzel', '2002-04-25', '2010-04-30', NULL, '25.23', '40.00', 'Hourly'),
+(37, 'Loren', 'R', 'Wetzel', '1981-08-09', '1999-08-01', NULL, '9.00', '20.00', 'Hourly'),
+(65, 'Aaron', 'A', 'Aaronson', '1980-01-01', '2013-01-02', '100000.00', NULL, NULL, 'Salary'),
+(66, 'Erin', 'E', 'Erinson', '1980-01-01', '2012-01-01', '75000.00', NULL, NULL, 'Salary'),
+(734, 'Roy', '', 'Batty', '2016-01-08', '2016-01-09', NULL, '15.86', '30.00', 'Hourly'),
+(1313, 'Beatrix', '', 'Kiddo', '1976-02-28', '2003-10-10', NULL, '13.50', '18.00', 'Hourly'),
+(1337, 'Molly', '', 'Millions', '1984-07-01', '2000-04-18', NULL, '19.25', '30.00', 'Hourly'),
+(1985, 'Marty', '', 'McFly', '1968-06-12', '1885-01-01', '50000.00', NULL, NULL, 'Salary'),
+(2000, 'Paul', 'Muad\'Dib', 'Atreides', '1965-04-04', '1984-05-05', NULL, '2.00', '29.00', 'Hourly'),
+(2001, 'Some', 'One', 'Else', '1999-07-07', '2018-08-27', NULL, NULL, NULL, 'None');
 
 --
 -- Indexes for dumped tables
@@ -75,7 +79,7 @@ ALTER TABLE `persons`
 -- AUTO_INCREMENT for table `persons`
 --
 ALTER TABLE `persons`
-  MODIFY `EmployeeID` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2002;
+  MODIFY `EmployeeID` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2008;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
